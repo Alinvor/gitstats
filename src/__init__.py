89c3acf9907b176d1edaa6eaaef45b03c3ae734f
zhenwei-li <zhenwei-li@qq.com>
2019-12-26 18:27:19 +0800

    数据收集器拆分

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+# The problem of introducing package directory into subsequent optimization
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
