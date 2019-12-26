adacd9d7bf5fe7835187bca953593f5e5746c4d4
zhenwei-li <zhenwei-li@qq.com>
2019-12-26 18:27:03 +0800

    配置信息添加

---
+++ src/com/dvsnier/configure/config.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+conf = {
+    'max_domains': 10,
+    'max_ext_length': 10,
+    'style': 'gitstats.css',
+    'max_authors': 20,
+    'authors_top': 5,
+    'commit_begin': '',
+    'commit_end': 'HEAD',
+    'linear_linestats': 1,
+    'project_name': '',
+    'processes': 8,
+    'start_date': '',
+    'expiry_date': ''
+}
