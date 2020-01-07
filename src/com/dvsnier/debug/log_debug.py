3e42d52355a87c20311bb26f6a0ce01f23d926e9
zhenwei-li <zhenwei-li@qq.com>
2020-01-07 14:10:05 +0800

    the log add write to files record

--- src/com/dvsnier/debug/log_debug.py
+++ src/com/dvsnier/debug/log_debug.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import os
+import datetime
 from com.dvsnier.debug.debug import Debug
 
 
@@ -34,6 +36,40 @@ class Log(Debug, object):
 
     def tips(self, msg):
         print("%s" % msg)
+
+    def write(self, file_name, msg, flag=0, ignore=True):
+        ''' the write data to files that use to append'''
+        if self.isDebug() and ignore:
+            return
+        else:
+            pass
+        log_dir = os.path.join(os.getcwd(), "log")
+        if not os.path.exists(log_dir):
+            try:
+                os.makedirs(log_dir)
+            except OSError:
+                pass
+
+        with open(os.path.join(log_dir, file_name), "a+") as file:
+            if msg:
+                file.write("[%s] %s\n" % (self.get_current_time_stamp(), msg))
+            else:
+                file.write("[%s] the current msg is empty.\n" %
+                           self.get_current_time_stamp())
+
+    def get_current_time_stamp(self):
+        ''' the obtain current system datetime '''
+        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    def obtain_time_stamp(self, style="%s", fmt="%Y%m%d_%H%M%S_%f"):
+        ''' the time stamp or get file name
+            eg:
+                    style              fmt
+                log_%s.log      %Y%m%d_%H%M%S_%f
+        '''
+        if style and style.find("%s") >= 0:
+            return (style % datetime.datetime.now().strftime(fmt))
+        return datetime.datetime.now().strftime(fmt)
 
 
 log = Log()
--- src/com/dvsnier/process/process.py
+++ src/com/dvsnier/process/process.py
@@ -14,14 +14,25 @@ def getpipeoutput(cmds, quiet=False):
     # global exectime_external
     exectime_external = 0.0
     start = time.time()
+    file_name = log.obtain_time_stamp("log_%s.log", "%Y%m%d_%H%M%S_%f")
+    log.write(file_name,
+              msg="the current file name is %s" % file_name,
+              ignore=False)
     if not quiet and ON_LINUX and os.isatty(1):
         print '>> ' + ' | '.join(cmds),
         sys.stdout.flush()
 
     log.log("the current execute commands: %s" % cmds)
+    log.write(file_name,
+              msg="the current execute commands: %s" % cmds,
+              ignore=False)
     p = subprocess.Popen(cmds[0], stdout=subprocess.PIPE, shell=True)
     processes = [p]
     for x in cmds[1:]:
+        log.log("the current execute sub commands: %s" % x)
+        log.write(file_name,
+                  msg="the current execute sub commands: %s" % x,
+                  ignore=False)
         p = subprocess.Popen(x,
                              stdin=p.stdout,
                              stdout=subprocess.PIPE,
@@ -37,4 +48,9 @@ def getpipeoutput(cmds, quiet=False):
         print '[%.5f] >> %s' % (end - start, ' | '.join(cmds))
     exectime_external += (end - start)
     set_exectime_external(exectime_external)
+    log.write(
+        file_name,
+        msg="the total time spent executing the current command(%s) is %s s." %
+        (cmds, str(exectime_external)),
+        ignore=False)
     return output.rstrip('\n')
