921a03ce4fd1ccb5d44fd75f9d9375a3f286f0df
zhenwei-li <zhenwei-li@qq.com>
2020-01-07 18:16:49 +0800

    the log debug test upgrade

--- src/com/dvsnier/debug/log_debug.py
+++ src/com/dvsnier/debug/log_debug.py
@@ -7,8 +7,13 @@ from com.dvsnier.debug.debug import Debug
 
 class Log(Debug, object):
     ''' the log debug '''
+
+    _default_log_name = ""
+
     def __init__(self):
         super(Log, self).__init__()
+        self.set_default_log_name(
+            self.obtain_time_stamp("%s.log", fmt="%Y%m%d%H%M%S.%f"))
 
     def v(self, tag, msg):
         if self.isDebug():
@@ -37,6 +42,26 @@ class Log(Debug, object):
     def tips(self, msg):
         print("%s" % msg)
 
+    def withOpen(self, base_dir, file_name, msg, mode="a+", ignore=True):
+        ''' the write the data to the file in the specified directory,
+         debug mode does not write the file '''
+        if self.isDebug() and ignore:
+            return
+        else:
+            pass
+        if not os.path.exists(base_dir):
+            return
+        with open(os.path.join(base_dir, file_name), mode) as file:
+            if msg:
+                file.write("[%s] %s\n" % (self.get_current_time_stamp(), msg))
+            else:
+                file.write("[%s] the current msg is empty.\n" %
+                           self.get_current_time_stamp())
+
+    def output(self, msg, flag=0, ignore=False):
+        ''' the local logging service records '''
+        self.write(self.get_default_log_name(), msg, flag, ignore)
+
     def write(self, file_name, msg, flag=0, ignore=True):
         ''' the write data to files that use to append'''
         if self.isDebug() and ignore:
@@ -49,13 +74,49 @@ class Log(Debug, object):
                 os.makedirs(log_dir)
             except OSError:
                 pass
+        self.withOpen(log_dir, file_name, msg, ignore=ignore)
 
-        with open(os.path.join(log_dir, file_name), "a+") as file:
-            if msg:
-                file.write("[%s] %s\n" % (self.get_current_time_stamp(), msg))
+    def writeToDir(self,
+                   file_name,
+                   msg,
+                   mode="a+",
+                   baseDir="log",
+                   subDir=None,
+                   flag=0,
+                   ignore=True,
+                   relative=True):
+        ''' the write data to directory files that use to append'''
+        if self.isDebug() and ignore:
+            return
+        else:
+            pass
+        base_dir = None
+        sub_dir = None
+        cwdir = None
+        if baseDir:
+            if relative:
+                base_dir = os.path.join(os.getcwd(), baseDir)
             else:
-                file.write("[%s] the current msg is empty.\n" %
-                           self.get_current_time_stamp())
+                base_dir = baseDir
+            if not os.path.exists(base_dir):
+                try:
+                    os.makedirs(base_dir)
+                except OSError:
+                    pass
+            cwdir = base_dir
+        if subDir:
+            sub_dir = os.path.join(base_dir, subDir)
+            # if relative:
+            #     sub_dir = os.path.join(base_dir, subDir)
+            # else:
+            #     sub_dir = os.path.join(base_dir, subDir)
+            if not os.path.exists(sub_dir):
+                try:
+                    os.makedirs(sub_dir)
+                except OSError:
+                    pass
+            cwdir = sub_dir
+        self.withOpen(cwdir, file_name, msg, mode=mode, ignore=ignore)
 
     def get_current_time_stamp(self):
         ''' the obtain current system datetime '''
@@ -70,6 +131,12 @@ class Log(Debug, object):
         if style and style.find("%s") >= 0:
             return (style % datetime.datetime.now().strftime(fmt))
         return datetime.datetime.now().strftime(fmt)
+
+    def set_default_log_name(self, log_name):
+        self._default_log_name = log_name
+
+    def get_default_log_name(self):
+        return self._default_log_name
 
 
 log = Log()
---
+++ test/com/dvsnier/debug/log_debug_test.py
@@ -0,0 +1,124 @@
+# -*- coding:utf-8 -*-
+
+import unittest
+
+import sys
+import os
+import random
+
+sys.path.append(os.path.join(os.getcwd(), "src"))
+sys.path.append(os.path.join(os.getcwd(), "mock"))
+
+from com.dvsnier.debug.log_debug import log
+
+
+class Test_Log(unittest.TestCase):
+    ''' the test log debug '''
+    @classmethod
+    def setUpClass(cls):
+        print("...the set up...")
+        print
+        log.setDebug(True)
+
+    def test_tips(self):
+        print
+        log.tips("the test tips() is succeed.")
+
+    def test_withOpen(self):
+        base_dir = os.path.join(os.getcwd(), "log")
+        file_name = "test_with_open.log"
+        msg = "这是一个测试数据,"
+        print
+        if not os.path.exists(base_dir):
+            try:
+                os.makedirs(base_dir)
+                print("the create dir(%s) is succeed." % base_dir)
+            except OSError as e:
+                print(e.message)
+            else:
+                print("the already files %s." % base_dir)
+        msg += str(random.randint(1, 100))
+        log.withOpen(base_dir, file_name, msg, mode='w+', ignore=False)
+        msg += str(random.randint(1, 100))
+        log.withOpen(base_dir, file_name, msg, ignore=False)
+        msg += str(random.randint(1, 100))
+        # log.withOpen(base_dir, file_name, msg, ignore=True)
+        log.withOpen(base_dir, file_name, msg, ignore=False)
+        log.tips("the test withOpen() is succeed.")
+
+    def test_output(self):
+        msg = "这是一个测试数据"
+        print
+        log.output(msg, ignore=False)
+        # log.output(msg, ignore=True)
+        log.tips("the test output() is succeed.")
+
+    def test_write(self):
+        # base_dir = os.path.join(os.getcwd(), "log")
+        file_name = "test_with_open.log"
+        msg = "这是一个测试数据,write"
+        print
+        log.write(file_name, msg, ignore=False)
+        # log.write(file_name, msg, ignore=True)
+        log.tips("the test write() is succeed.")
+
+    def test_writeToDir(self):
+        # relative False
+        # base_dir = os.path.join(os.getcwd(), "log")
+
+        # relative True
+        base_dir = "log"
+
+        sub_dir = "thread"
+        file_name = "test_with_thread_open.log"
+        msg = "这是一个测试数据,write"
+        print
+        log.writeToDir(file_name,
+                       msg,
+                       mode="a+",
+                       baseDir=base_dir,
+                       subDir=sub_dir,
+                       ignore=False,
+                       relative=False)
+        # log.writeToDir(
+        #     file_name,
+        #     msg,
+        #     mode="a+",
+        #     # baseDir=base_dir,
+        #     subDir=sub_dir,
+        #     # ignore=True,
+        #     ignore=False,
+        #     relative=False)
+        log.tips("the test writeToDir() is succeed.")
+
+    def test_get_current_time_stamp(self):
+        print
+        print log.get_current_time_stamp()
+        log.tips("the test get_current_time_stamp() is succeed.")
+
+    def test_obtain_time_stamp(self):
+        print
+        print log.obtain_time_stamp()
+        print log.obtain_time_stamp(style="log_%s.log")
+        print log.obtain_time_stamp(style="%s.bat", fmt="%Y%m%d_%H%M%S")
+        log.tips("the test obtain_time_stamp() is succeed.")
+
+    def test_set_default_log_name(self):
+        print
+        log.set_default_log_name("test.log")
+        self.test_get_default_log_name()
+        log.tips("the test set_default_log_name() is succeed.")
+
+    def test_get_default_log_name(self):
+        print
+        print log.get_default_log_name()
+        log.tips("the test get_default_log_name() is succeed.")
+
+    @classmethod
+    def tearDownClass(cls):
+        print
+        print("...the tear down...")
+
+
+if __name__ == '__main__':
+    unittest.main()
