4c8deeaa39946b0e21cc4f5730a34e0d0a8de7c0
zhenwei-li <zhenwei-li@qq.com>
2020-03-02 13:13:45 +0800

    the test parameter correction

--- mock/com/dvsnier/mock/sys_argv.py
+++ mock/com/dvsnier/mock/sys_argv.py
@@ -2,13 +2,22 @@
 
 # the mock sys argvs list
 _sys_argvs = []
+project_name = 'gitstats'
+start_date = '2019-12-26'
+expiry_date = '2020-01-30'
 
 sys_argv_0 = [
-    '-c', 'start_date=2019-12-26', '-e', 'expiry_date=2020-01-30',
-    '../gitstats', './out/gitstats'
+    '-c', 'start_date={start_date}'.format(start_date=start_date), '-e',
+    'expiry_date={expiry_date}'.format(expiry_date=expiry_date),
+    '../{project}'.format(project=project_name),
+    './out/{project}'.format(project=project_name)
 ]
 
-sys_argv_1 = ['-c', 'start_date=2019-12-01', '../gitstats', './out/gitstats']
+sys_argv_1 = [
+    '-c', 'start_date={start_date}'.format(start_date=start_date),
+    '../{project}'.format(project=project_name),
+    './out/{project}'.format(project=project_name)
+]
 
 _sys_argvs.append(sys_argv_0)
 _sys_argvs.append(sys_argv_1)
